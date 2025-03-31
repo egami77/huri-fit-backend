70269b4d735318314e412281022653309bb8e58f
image gautam <gautamimage7@gmail.com>
2025-03-31 12:05:26 +0545

    added login and backend api

--- Auth/server.js
+++ Auth/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./dbconnect.js";
+import router from "./HURI-FIT/routes/authRoutes.js";
 // const express = require("express");
 // const cors = require("cors");
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/auth", router);
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
 });
